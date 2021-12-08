85c169743dbb61a994903a1b08ebc7f99f565570
RandyF <aressa@angrykitten.com>
2021-12-08 12:54:35 -0800

    Completed Day 08

---
+++ 2021/AoC_2021_template.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+import numpy as np
+
+from AoC_Utils import debug
+from AoC_2021_Submarine import Submarine
+
+from AoC_2021_SegmentedDisplay import *
+
+#=========================================================================================
+# Day Class
+#=========================================================================================
+class AoC_2021():
+    Verbosity = None
+    PuzzleData = None
+    OurSub = None
+
+    #------------------------------------------------------------------------------
+    # Initialize Puzzle Day
+    #------------------------------------------------------------------------------
+    def __init__(self, FileBase, UseSample=False, Verbosity=0):
+        self.Verbosity = Verbosity
+        self.PuzzleData = []
+        self.OurSub = Submarine(Verbosity=Verbosity)
+        
+        if FileBase != None:
+            self.ReadAndParsePuzzleData(FileBase, UseSample)
+
+
+    #------------------------------------------------------------------------------
+    # Read Puzzle Data and do any simple parsing
+    #------------------------------------------------------------------------------
+    def ReadAndParsePuzzleData(self, FileBase, UseSample=False):
+
+        tFileName = FileBase
+
+        if UseSample:
+            tFileName += '_Sample'
+
+        tFile = open('%s.txt' % tFileName, 'r')
+
+        #PARSE DATA HERE
+
+        tFile.close()
+
+
+    def PartA(self):
+
+       return 0
+
+
+    def PartB(self):
+
+       return 0
+
+
+if __name__== "__main__":
+
+    AoC_Puzzle = AoC_2021( u'_inputs/2021_xx_Data', UseSample=False, Verbosity=1)
+
+    print(AoC_Puzzle.PartA()) # x
+    print(AoC_Puzzle.PartB()) # x
